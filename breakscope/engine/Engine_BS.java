2025ed9047cd5e7215557d5ad4bf7e94132563b7
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2019-02-15 19:51:54 +0800

    DefaultStageEditor統一
    ・Editorの内容がTHHとBreakScopeIIでかぶってるのでデフォルト化。
    ・編集モード中でも視点移動が可能（モードを戻すとプレーヤーがサークル内へ転移する仕様が残っている）

--- breakscope/engine/Engine_BS.java
+++ breakscope/engine/Engine_BS.java
@@ -15,14 +15,11 @@ import chara.Player;
 import chara.WhiteMan;
 import core.GHQ;
 import core.MessageSource;
-import gui.BasicButton;
-import paint.ColorFraming;
-import paint.ImageFrame;
-import paint.PaintScript;
+import gui.DefaultStageEditor;
 import stage.ControlExpansion;
 import stage.StageEngine;
+import stage.StageSaveData;
 import structure.Structure;
-import structure.Terrain;
 import unit.*;
 
 public class Engine_BS extends StageEngine implements MessageSource,ActionSource{
@@ -73,7 +70,7 @@ public class Engine_BS extends StageEngine implements MessageSource,ActionSource
 		vegImageIID[2] = GHQ.loadImage("veg_leaf2.png");
 		vegImageIID[3] = GHQ.loadImage("veg_stone.png");
 		vegImageIID[4] = GHQ.loadImage("veg_leaf3.png");
-		Editor.loadResource();
+		DefaultStageEditor.init(new File("stage/saveData1.txt"));
 	}
 	@Override
 	public final void charaSetup() {
@@ -106,6 +103,10 @@ public class Engine_BS extends StageEngine implements MessageSource,ActionSource
 	@Override
 	public final void openStage() {
 		GHQ.addMessage(this,"This is a prototype stage.");
+	}
+	@Override
+	public final StageSaveData getStageSaveData() {
+		return new Stage_BS(GHQ.getCharacters(),GHQ.getStructures());
 	}
 	//idle
 	private int gameFrame;
@@ -188,23 +189,23 @@ public class Engine_BS extends StageEngine implements MessageSource,ActionSource
 		if(ctrlEx.pullCommandBool(CtrlEx_BS.EDIT_MODE)) {
 			if(editMode) {
 				editMode = false;
-				GHQ.disableGUIs(Editor.EDIT_MODE_GROUP);
+				GHQ.disableGUIs(DefaultStageEditor.EDIT_MODE_GROUP);
 				GHQ.clearStopEvent();
 			}else if(GHQ.isNoStopEvent()) {
 				editMode = true;
-				GHQ.enableGUIs(Editor.EDIT_MODE_GROUP);
+				GHQ.enableGUIs(DefaultStageEditor.EDIT_MODE_GROUP);
 				GHQ.stopScreen_noAnm();
 			}
 		}
 		if(editMode) {
-			Editor.idle(g2);
+			DefaultStageEditor.idle(g2);
 		}else { //game GUI
 			GHQ.translateForGUI(true);
 			int pos = 1;
 			GHQ.drawImageGHQ(player.faceIID, pos++*90 + 10, GHQ.getScreenH() - 40, 80, 30);
 			GHQ.translateForGUI(false);
 		}
-		if(stopEventKind == NONE) { //scroll
+		if(stopEventKind == NONE || editMode) { //scroll
 			//scroll by keys
 			if(ctrlEx.getCommandBool(CtrlEx_BS.UP)) {
 				formationCenterY -= F_MOVE_SPD;
@@ -266,113 +267,4 @@ public class Engine_BS extends StageEngine implements MessageSource,ActionSource
 		return stageH;
 	}
 	private boolean doScrollView = true;
-	
-	//Editor///////////////////////////////////
-	
-	private static final class Editor{
-		//gui
-		
-		private static int placeX,placeY;
-		
-		private static final byte
-			NOTHING = -1,
-			TERRAIN = 0,
-			TILES = 1,
-			ENEMY = 2,
-			ITEM = 3;
-		private static byte placeKind = NOTHING;
-		//GUI_GROUP_ID
-		private static final int
-			EDIT_MODE_GROUP = 0;
-		//PaintScripts
-		private static final PaintScript RED_FRAMING = new ColorFraming(Color.RED,GHQ.stroke3);
-		//loadResource
-		static void loadResource() {
-			final int SCREEN_W = GHQ.getScreenW(),SCREEN_H = GHQ.getScreenH();
-			GHQ.addGUIParts(new BasicButton(EDIT_MODE_GROUP,null,150 + (SCREEN_W - 150)/2,SCREEN_H/2,SCREEN_W - 150,SCREEN_H) {
-				@Override
-				public void clicked() {
-					switch(placeKind) {
-					case TERRAIN:
-						if(Terrain.blueprint_isOriginPoint(placeX, placeY))
-							GHQ.addStructure(Terrain.blueprint_flush());
-						else
-							Terrain.blueprint_addPoint(placeX, placeY);
-						break;
-					case TILES:
-						break;
-					case ENEMY:
-						break;
-					case ITEM:
-						break;
-					}
-				}
-			});
-			GHQ.addGUIParts(new BasicButton(EDIT_MODE_GROUP,new ImageFrame("gui_editor/Tiles.png"),55,155,40,40) {
-				@Override
-				public void clicked() {
-					placeKind = (placeKind == TILES ? NOTHING : TILES);
-				}
-				@Override
-				public void paint() {
-					super.paint();
-					if(placeKind == TILES)
-						RED_FRAMING.paint(x, y, w, h);
-				}
-			});
-			GHQ.addGUIParts(new BasicButton(EDIT_MODE_GROUP,new ImageFrame("gui_editor/FreeShape.png"),100,155,40,40) {
-				@Override
-				public void clicked() {
-					placeKind = (placeKind == TERRAIN ? NOTHING : TERRAIN);
-				}
-				@Override
-				public void paint() {
-					super.paint();
-					if(placeKind == TERRAIN)
-						RED_FRAMING.paint(x, y, w, h);
-				}
-			});
-			GHQ.addGUIParts(new BasicButton(EDIT_MODE_GROUP,new ImageFrame("gui_editor/Save.png"),77,500,85,40) {
-				@Override
-				public void clicked() {
-					System.out.println("saving...");
-					GHQ.saveData(new Stage_BS(GHQ.getCharacters(),GHQ.getStructures()),new File("stage/saveData1.txt"));
-					System.out.println("complete!");
-				}
-			});
-		}
-		//role
-		static void idle(Graphics2D g2) {
-			//mouse
-			if(ctrlEx.getCommandBool(CtrlEx_BS.LEAP)) {
-				g2.setColor(Color.RED);
-				final int N = 100;
-				int S = 4;
-				final int SX = (GHQ.getMouseX() + N/2)/N,SY = (GHQ.getMouseY() + N/2)/N;
-				for(int xi = -1;xi <= +1;xi++) {
-					for(int yi = -1;yi <= +1;yi++)
-						g2.fillOval((SX + xi)*N - S/2, (SY + yi)*N - S/2, S, S);
-				}
-				S += 6;
-				g2.drawOval(SX*N - S/2, SY*N - S/2, S, S);
-				placeX = SX*N;
-				placeY = SY*N;
-			}else {
-				placeX = GHQ.getMouseX();
-				placeY = GHQ.getMouseY();
-			}
-			//origin
-			switch(placeKind) {
-			case TERRAIN:
-				Terrain.blueprint_markPoints(new ColorFraming(Color.ORANGE,GHQ.stroke1,4,4));
-				Terrain.blueprint_markOrigin(new ColorFraming(Color.ORANGE,GHQ.stroke1,8,8));
-				break;
-			}
-			//gui
-			GHQ.translateForGUI(true);
-			g2.setColor(Color.WHITE);
-			g2.drawString("EDIT_MODE", 20, 20);
-			GHQ.translateForGUI(false);
-		}
-	}
 }
